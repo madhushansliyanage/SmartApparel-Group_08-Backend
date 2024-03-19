54927599ff30c519a3de18b26568aced08fb2b4e
Hasindu1219 <maneth_2020735@fot.ruh.ac.lk>
2024-03-19 21:52:33 +0530

    initial project

---
+++ SmartApparel/src/main/java/com/example/SmartApparel/SmartApparelApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartApparel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartApparelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartApparelApplication.class, args);
+	}
+
+}
